33c07bbbb15a6f5bd81f4f094f4272dd5bff691e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-08 23:07:26 -0400

    Added a method to the object

--- loginstatus.js
+++ loginstatus.js
@@ -1,10 +1,16 @@
 // object, properties, methods (functions), end of object, function (with parameters),  
 
 let logInInfo = {
-    currentUserName = "",
-    currentEmail = "",
-    userNamesLoggedIn = [],
-    emailsLoggedIn = [],
-    userNamesLoggedOut = [],
-    emailsLoggedOut = [],
-}
+    currentUserNamez: "",
+    currentEmail: "",
+    userNamesLoggedIn: [],
+    emailsLoggedIn: [],
+    userNamesLoggedOut: [],
+    emailsLoggedOut: [],
+    logIn: function() {
+        this.userNamesLoggedIn.push(this.currentUserName);
+        this.emailsLoggedIn.push(this.currentEmail);
+        console.log(`Welcome ${this.currentUserName[i]}!`);
+        console.log(`You have successfully logged in with your email ${this.currentEmail[i]}!`);
+    }
+},
